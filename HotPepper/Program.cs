7ab1ffa843592c1d44485676764fdae5028e1d65
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-15 23:03:55 -0500

    initial commit

---
+++ HotPepper/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace HotPepper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "Hot Pepper!";
+            //MainMenu.Start();
+        }
+    }
+}
