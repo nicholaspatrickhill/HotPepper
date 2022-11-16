6860f2a14daf9b97f65556df9202bfd14e2814ab
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-15 23:07:09 -0500

    added some basic menu functionality

---
+++ HotPepper/MainMenu.cs
@@ -0,0 +1,75 @@
+﻿using static System.Console;
+
+
+namespace HotPepper
+{
+    public class MainMenu
+    {
+        public static void Start()
+        {
+            var mainMenu = new MainMenu();
+            mainMenu.RunMainMenu();
+        }
+
+        private void RunMainMenu()
+        {
+            string prompt = "Use the UP and DOWN arrow keys to highlight the pepper you with to review and press enter.\n";
+            string[] options = { "Bell Pepper", "Carolina Reaper", "Fatalii", "Fish Pepper", "Ghost Pepper", "Habanero",
+            "Jalapeno", "Lemon Drop", "Poblano", "Scotch Bonnet", "Serrano", "Tabasco", "Trinidad Scorpion", "Exit"};
+            Menu mainMenu = new Menu(prompt, options);
+            int selectedIndex = mainMenu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                    ExitTheProgram();
+                    break;
+            }
+        }
+
+
+
+        private static void ExitTheProgram()
+        {
+            ForegroundColor = ConsoleColor.White;
+            WriteLine();
+            WriteLine("Are you sure you wish to exit the program?");
+            WriteLine("Type \"y\" and press enter to exit or type \"n\" and press enter to return to the Main Menu.");
+
+            ForegroundColor = ConsoleColor.Cyan;
+            string input = ReadLine() ?? String.Empty;
+
+            switch (input.ToLower())
+            {
+                case "y":
+                    ForegroundColor = ConsoleColor.White;
+                    Environment.Exit(0);
+                    break;
+                case "n":
+                    Start();
+                    break;
+                default:
+                    ForegroundColor = ConsoleColor.White;
+                    WriteLine();
+                    WriteLine("Invalid Command. Please try again.");
+                    ExitTheProgram();
+                    break;
+            }
+        }
+    }
+}
---
+++ HotPepper/Menu.cs
@@ -0,0 +1,77 @@
+﻿using static System.Console;
+
+namespace HotPepper
+{
+    public class Menu
+    {
+        private int SelectedIndex;
+        private string[] Options;
+        private string Prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 0;
+        }
+
+        private void DisplayOptions()
+        {
+            ForegroundColor = ConsoleColor.White;
+
+            //ConsoleMessages.PrintAppHeader();
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+
+                if (i == SelectedIndex)
+                {
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+
+                WriteLine(currentOption);
+            }
+            ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
+    }
+}
